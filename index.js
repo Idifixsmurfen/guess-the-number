52e90742845c57e9a9ac7d1d9b4dde812d657b68
walter <wdetho01@gmail.com>
2020-11-02 15:51:18 +0100

    klar2

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Read instructions. And remove this line.
+const message = document.getElementById('message')
+const inputField = document.getElementById("user-input")
+
+function RandomInt()
+{
+    return Math.floor(Math.random() * 101)
+    
+}
+let SecretNumber = RandomInt() 
+
+
+document.addEventListener("keyup", function(event)
+{
+    if(event.key == "Enter")
+    {
+        const guess = GetUserInput()
+
+        if(GetUserInput() < SecretNumber)
+        {
+            SetMessage("Higher damnit!")
+        }
+        else if (GetUserInput() > SecretNumber)
+        {
+            SetMessage("Lower you madman!")
+        }
+        else
+        {
+            SetMessage("You did great son :) ")
+
+            setTimeout(function() {location.reload()}, 5000)
+        }
+    }
+
+}
+
+
+)
+
+
+function GetUserInput()
+{
+    return parseInt(inputField.value);
+}
+function SetMessage(msg)
+{
+    message.innerHTML = msg
+}
+console.log(SecretNumber)
